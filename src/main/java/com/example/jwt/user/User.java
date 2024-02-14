820cc8600b9fa968b0010b24667cdd17ebee62cb
hamsu100 <hms1004t@naver.com>
2024-02-15 00:04:13 +0900

    Init Project 1. pom.xml 구성 2. application.yml 구성 3. User Entity 생성

---
+++ src/main/java/com/example/jwt/user/User.java
@@ -0,0 +1,26 @@
+package com.example.jwt.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String password;
+}
